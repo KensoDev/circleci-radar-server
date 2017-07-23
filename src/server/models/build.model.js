3948946d658d3a555b3ad1f65a93e44c414b7f3b
KensoDev <avi@kensodev.com>
2017-07-22 21:20:19 -0700

    Build model with more items

--- src/server/models/build.model.js
+++ src/server/models/build.model.js
@@ -3,5 +3,7 @@ export default class Build {
 		this.name = name;
 		this.status = buildObject.status;
 		this.build_num = buildObject.build_num;
+    this.build_url = buildObject.build_url;
+    this.commit_url = buildObject.commit_url;
   }
 }
